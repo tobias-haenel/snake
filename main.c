b5f098c17240b43cdcee08330a3be12a45bf924c
Tobias Hänel <contact@tobias-haenel.de>
2018-01-25 13:37:55 +0100

    Fixed logic error in inital interface

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ struct snake_body_part_t *
 init_snake(int fieldWidth, int fieldHeight);
 
 void
-move_snake(struct snake_body_part_t *snake);
+move_snake(struct game_state_t *game_state);
 
 void
 deinit_snake(struct snake_body_part_t *snake);
@@ -49,20 +49,29 @@ struct food_t *
 init_food();
 
 void
-spawn_food(struct food_t *food);
+spawn_food(struct game_state_t *game_state);
 
 void
 deinit_food(struct food_t *food);
 
+void
+read_inputs(struct game_state_t *game_state);
+
+void
+update_state(struct game_state_t *game_state);
+
+void
+render_state(struct game_state_t *game_state);
+
 int main() {
     // init
-    while (true) {
+    struct game_state_t *game_state = init_game_state();
+    for(;;) {
         read_inputs(game_state);
-        while (...) {
-            update_state(game_state);
-        }
+        update_state(game_state);
         render_state(game_state);
     }
     // deinit
+    deinit_game_state(game_state);
     return 0;
 }
